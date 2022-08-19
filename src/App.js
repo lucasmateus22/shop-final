1a9ae014c404077bf634130bae142e78c3aa6dab
Pedro Henrique <pedro.andrade@nave.org.br>
2022-08-19 20:22:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./componentes/Header";
+import Filter from "./componentes/Filter"
+
+function App(props) {
+  return (
+    <div className="App">
+     <Header/>
+     <Filter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style.css"
+
+export default function Header(Props){
+    return(
+
+        <div className="header">
+            <img href=""></img>
+            <input className="busca"></input>
+            <button className="buscaBut"><strong>Busca</strong></button>
+
+        </div>
+    );
+}
